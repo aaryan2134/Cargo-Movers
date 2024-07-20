4740809244210b4aade273a788cd35acf1d29844
aaryan2134 <aaryan2134@gmail.com>
2024-07-21 01:50:57 +0530

    made changes to website

--- js/timeline.js
+++ js/timeline.js
@@ -1,51 +1,37 @@
-$(function(){
+$(document).ready(function(){
 
   window.sr = ScrollReveal();
 
   if ($(window).width() < 768) {
+    // Update class if necessary
+    $('.timeline-content').each(function() {
+      if ($(this).hasClass('js--fadeInLeft')) {
+        $(this).removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+      }
+    });
 
-  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-  	}
-
-  	sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    // ScrollReveal initialization for smaller screens
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
   } else {
-  	
-  	sr.reveal('.js--fadeInLeft', {
-	    origin: 'left',
-	    distance: '300px',
-		  easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    // ScrollReveal initialization for larger screens
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
-	  sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
-
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
   }
-  
-  sr.reveal('.js--fadeInLeft', {
-	    origin: 'left',
-	    distance: '300px',
-		  easing: 'ease-in-out',
-	    duration: 800,
-	  });
-
-	  sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
-
-
 });
