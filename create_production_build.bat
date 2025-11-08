f635ae3c0078afbaef0f08a48f83cc9c835b5aa3
aaryan2134 <aaryan2134@gmail.com>
2025-11-09 04:17:58 +0530

    cleanup images and files

---
+++ create_production_build.bat
@@ -0,0 +1,38 @@
+@echo off
+echo Creating production build directory...
+
+set BUILD_DIR=production_upload
+
+REM Clean up previous build
+if exist %BUILD_DIR% (
+    echo Removing old build directory...
+    rmdir /s /q %BUILD_DIR%
+)
+
+mkdir %BUILD_DIR%
+mkdir %BUILD_DIR%\css
+mkdir %BUILD_DIR%\css\fonts
+mkdir %BUILD_DIR%\js
+mkdir %BUILD_DIR%\dist
+
+echo Copying essential files...
+
+REM Copy HTML and component files
+copy *.html %BUILD_DIR%\
+
+REM Copy minified assets and fonts
+copy css\production.min.css %BUILD_DIR%\css\
+copy css\fonts\* %BUILD_DIR%\css\fonts\
+copy js\production.min.js %BUILD_DIR%\js\
+
+REM Copy optimized images and assets from dist
+xcopy /s /e /i dist %BUILD_DIR%\dist\
+
+REM Copy SEO files
+copy robots.txt %BUILD_DIR%\
+copy sitemap.xml %BUILD_DIR%\
+
+echo.
+echo Production build created in '%BUILD_DIR%' directory.
+echo You can now upload the contents of this folder to your Plesk server.
+pause
